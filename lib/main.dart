a13816f985d6134eeb8fe210235a3734dc31bb10
oliverwstein <oliversteininc@gmail.com>
2023-12-31 13:12:54 -0500

    Added zoom-in/zoom-out functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui' as ui;
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flame/sprite.dart';
@@ -15,33 +14,82 @@ enum Direction {
   right
 }
 
-class MyStage extends Component with HasGameRef<FlameGame> {
-  MyStage();
+class Stage extends Component with HasGameRef<MyGame> {
+  Stage();
 
   @override
   Future<void> onLoad() async {
     // Load the tiled map and set its anchor to the top-left.
     final tiledMap = await TiledComponent.load('Ch0.tmx', Vector2.all(16));
     tiledMap.anchor = Anchor.topLeft;
+    tiledMap.scale = Vector2.all(MyGame().scaleFactor);
     
     // Add the tiled map to the stage.
     add(tiledMap);
   }
+  @override
+  void onMount() {
+    super.onMount();
+    gameRef.addObserver(this);
+  }
+
+  @override
+  void onRemove() {
+    gameRef.removeObserver(this);
+    super.onRemove();
+  }
+
+  @override
+  void onScaleChanged(double scaleFactor) {
+    for (final child in children) {
+      if (child is TiledComponent) {
+        child.scale = Vector2.all(scaleFactor);
+      }
+    }
+  }
+}
+abstract class ScaleObserver {
+  void onScaleChanged(double scaleFactor);
 }
 
 class MyGame extends FlameGame with KeyboardEvents {
+  late FixedAspectRatioViewport viewport;
+  late Stage stage;
   late Cursor cursor;
+  double _scaleFactor = 1;
+  final List _observers = [];
+  double get scaleFactor => _scaleFactor;
+
+  set scaleFactor(double value) {
+    if (_scaleFactor != value) {
+      _scaleFactor = value;
+      for (var observer in _observers) {
+        observer.onScaleChanged(_scaleFactor);
+      }
+    }
+  }
+
+  void addObserver(observer) {
+    _observers.add(observer);
+  }
+
+  void removeObserver(observer) {
+    _observers.remove(observer);
+  }
 
   @override
   Future<void> onLoad() async {
+    
     await super.onLoad();
     // Your existing onLoad implementation
-    await add(MyStage());
-    ui.Image cursorImage = await Flame.images.load('cursor.png');
-    SpriteSheet cursorSheet = SpriteSheet.fromColumnsAndRows(image: cursorImage,
-      columns: 3, rows: 1, spacing: 0);
-    cursor = Cursor(SpriteAnimationComponent(animation:cursorSheet.createAnimation(row: 0, stepTime: .2), size:Vector2.all(16)));
+    viewport = FixedAspectRatioViewport(aspectRatio: canvasSize.x/canvasSize.y);
+    camera.viewport = viewport;
+    stage = Stage();
+    await add(stage);
+    addObserver(stage);
+    cursor = Cursor();
     add(cursor);
+    addObserver(cursor);
   }
 
   @override
@@ -62,6 +110,12 @@ class MyGame extends FlameGame with KeyboardEvents {
       } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
         cursor.move(Direction.down);
         handled = true;
+      } else if (event.logicalKey == LogicalKeyboardKey.equal) { // "+" key for zooming in
+        scaleFactor *= 1.1;//.clamp(0.5, 2.0); // Zoom in and clamp between 0.5x and 2x
+        handled = true;
+      } else if (event.logicalKey == LogicalKeyboardKey.minus) { // "-" key for zooming out
+        scaleFactor *= 0.9;//.clamp(0.5, 2.0); // Zoom out and clamp
+        handled = true;
       }
     }
 
@@ -70,35 +124,45 @@ class MyGame extends FlameGame with KeyboardEvents {
 }
 
 void main() {
-  // Define the desired size of your game.
-  final gameSize = Vector2(1024, 512); // Change this to your desired game size.
-
-  // Create a FixedResolutionViewport with the gameSize.
-  final viewport = FixedResolutionViewport(resolution: gameSize);
-
-  // Instantiate your game with the viewport.
-  final game = MyGame()
-    ..camera.viewport = viewport; // Attach the viewport to the camera as well.
-
-  // Run the app with your game.
+  final game = MyGame();
   runApp(
     GameWidget(game: game),
   );
 }
 
-class Cursor extends PositionComponent {
+class Cursor extends PositionComponent with HasGameRef<MyGame> {
   late final SpriteAnimationComponent _animationComponent;
+  late final SpriteSheet cursorSheet;
   // The size of each tile in your map
-  static const int tileSize = 16;
   // The cursor's position in terms of tiles, not pixels
   Point tilePosition = Point(x:4, y:15);
+  late double tileSize;
+  Cursor() {
+    // Initial size, will be updated in onLoad
+    tileSize = 16 * MyGame().scaleFactor;
+  }
 
-  Cursor(SpriteAnimationComponent animationComponent) {
-    width = height = tileSize.toDouble(); // Make the cursor the same size as a tile
-    x = tilePosition.x*tileSize;
-    y = tilePosition.y*tileSize;
-    _animationComponent = animationComponent;
+  @override
+  Future<void> onLoad() async {
+    // Load the cursor image and create the animation component
+    ui.Image cursorImage = await gameRef.images.load('cursor.png');
+    cursorSheet = SpriteSheet.fromColumnsAndRows(
+      image: cursorImage,
+      columns: 3,
+      rows: 1,
+    );
+
+    _animationComponent = SpriteAnimationComponent(
+      animation: cursorSheet.createAnimation(row: 0, stepTime: .2),
+      size: Vector2.all(tileSize), // Use tileSize for initial size
+    );
+
+    // Add the animation component as a child
     add(_animationComponent);
+
+    // Set the initial size and position of the cursor
+    size = Vector2.all(tileSize);
+    position = Vector2(tilePosition.x * tileSize, tilePosition.y * tileSize);
   }
 
   // A method to move the cursor
@@ -120,6 +184,28 @@ class Cursor extends PositionComponent {
     // Update the pixel position of the cursor
     x = tilePosition.x * tileSize;
     y = tilePosition.y * tileSize;
+  }
+
+  @override
+  void onMount() {
+    super.onMount();
+    gameRef.addObserver(this);
+  }
+
+  @override
+  void onRemove() {
+    gameRef.removeObserver(this);
+    super.onRemove();
+  }
+  
+  @override
+  void onScaleChanged(double scaleFactor) {
+    tileSize = 16 * scaleFactor; // Update tileSize
+    size = Vector2.all(tileSize); // Update the size of the cursor itself
+    _animationComponent.size = Vector2.all(tileSize); // Update animation component size
+
+    // Update position based on new tileSize
+    position = Vector2(tilePosition.x * tileSize, tilePosition.y * tileSize);
   }
 
 }
