00db6f6b2a500c6c79a3da2927f637427918cec0
oliverwstein <oliversteininc@gmail.com>
2024-01-01 17:44:08 -0500

    Revised class Tile.

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,52 @@ import 'package:flame/sprite.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-enum Direction {
-  up,
-  down,
-  left,
-  right
-}
-
-class Tile {
-  final Point position;
+enum Direction {up, down, left, right}
+enum TileState {blank, move, attack}
+class Tile extends PositionComponent with HasGameRef<MyGame>{
+  late final SpriteAnimationComponent _moveAnimationComponent;
+  late final SpriteAnimationComponent _attackAnimationComponent;
+  late final SpriteSheet movementSheet;
+  late final SpriteSheet attackSheet;
+  late final Point gridCoord;
+  late double tileSize;
   String terrainType; // e.g., "grass", "water", "mountain"
   Unit? unit; // Initially null, set when a unit moves into the tile
+  TileState state = TileState.blank;
 
-  Tile(this.position, this.terrainType);
+  Tile(Point gridCoord, this.terrainType){
+    tileSize = 16 * MyGame().scaleFactor;
+  }
+  @override
+  Future<void> onLoad() async {
+    // Load the cursor image and create the animation component
+    ui.Image moveImage = await gameRef.images.load('movement_highlight.png');
+    ui.Image attackImage = await gameRef.images.load('attack_highlight.png');
+    movementSheet = SpriteSheet.fromColumnsAndRows(
+      image: moveImage,
+      columns: 2,
+      rows: 1,
+    );
+    attackSheet = SpriteSheet.fromColumnsAndRows(
+      image: attackImage,
+      columns: 2,
+      rows: 1,
+    );
+
+    _moveAnimationComponent = SpriteAnimationComponent(
+      animation: movementSheet.createAnimation(row: 0, stepTime: .2),
+      size: Vector2.all(tileSize), // Use tileSize for initial size
+    );
+
+    _attackAnimationComponent = SpriteAnimationComponent(
+      animation: attackSheet.createAnimation(row: 0, stepTime: .2),
+      size: Vector2.all(tileSize), // Use tileSize for initial size
+    );
+
+    add(_moveAnimationComponent);
+    add(_attackAnimationComponent);
+    position = Vector2(gridCoord.x * tileSize, gridCoord.y * tileSize);
+  }
 
   bool get isOccupied => unit != null;
 
@@ -33,6 +66,24 @@ class Tile {
   void removeUnit() {
     unit = null;
   }
+
+  @override
+void render(Canvas canvas) {
+  super.render(canvas); // Don't forget to call super.render
+  switch(state) {
+    case TileState.blank:
+      // Do nothing
+      break;
+    case TileState.move:
+      // Render move animation component
+      _moveAnimationComponent.render(canvas);
+      break;
+    case TileState.attack:
+      // Render attack animation component
+      _attackAnimationComponent.render(canvas);
+      break;
+  }
+}
 }
 
 class Stage extends Component with HasGameRef<MyGame>{
