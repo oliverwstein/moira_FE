c9c08834ccef51db894fdadfe369017776ea01f1
oliverwstein <oliversteininc@gmail.com>
2024-01-05 12:52:58 -0500

    Adjusted weapon.dart to work with any name.

--- lib/engine/weapon.dart
+++ lib/engine/weapon.dart
@@ -15,11 +15,9 @@ class Weapon extends Component {
   late List<CombatEffect>? effects; // The special effects of the weapon. 
   late Attack? specialAttack; // The special attack that can be performed with the weapon, if any. 
 
-  Weapon.fromJson(String jsonString) {
-    Map<String, dynamic> jsonMap = jsonDecode(jsonString);
-
+  Weapon.fromJson(String name, String jsonString) {
     // Define default values
-    Map<String, dynamic> defaultValues = {
+    Map<String, dynamic> weaponData = {
       "might": 0,
       "hit": 0,
       "fatigue": 0,
@@ -27,13 +25,19 @@ class Weapon extends Component {
       "effects": [],
       "specialAttack": null,
     };
+    Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+    if (jsonMap['weapons'].contains_key(name)){
+      weaponData = jsonMap['weapons'][name];
+    } else {
+      weaponData['weaponType'] = "none";
+    }
 
     // Get the weapon type first
-    var weaponTypeString = jsonMap['weaponType']; // Ensure this key exists or have error handling
+    var weaponTypeString = weaponData['weaponType']; // Ensure this key exists or have error handling
     weaponType = WeaponType.values.firstWhere((wt) => wt.toString().split('.').last == weaponTypeString);
 
     // Use the weapon type to retrieve the default values for this weapon type
-    var defaults = defaultValues[weaponTypeString] ?? {};
+    var defaults = weaponData[weaponTypeString] ?? {};
 
     // Assign values from JSON or use defaults
     might = jsonMap['might'] ?? defaults['might'] ?? 0;
@@ -41,6 +45,8 @@ class Weapon extends Component {
     fatigue = jsonMap['fatigue'] ?? defaults['fatigue'] ?? 0;
     magic = jsonMap['magic'] ?? defaults['magic'] ?? false;
     effects = jsonMap['effects'] ?? defaults['effects'] ?? [];
-    specialAttack = jsonMap['specialAttack'] ?? defaults['specialAttack'];
+    String specialAttackName = jsonMap['specialAttack'] ?? defaults['specialAttack'];
+    specialAttack = Attack.fromJson(specialAttackName, 'moira/assets/data/attacks.json');
+
   }
 }
