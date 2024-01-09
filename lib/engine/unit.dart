affe55490c6bf358abcd446e9d6c5991ce62e6f1
oliverwstein <oliversteininc@gmail.com>
2024-01-09 11:28:51 -0500

    Implementing classes with skills and weapon proficiencies.

--- lib/engine/unit.dart
+++ lib/engine/unit.dart
@@ -50,6 +50,7 @@ class Unit extends PositionComponent with HasGameRef<MyGame> implements CommandH
   Map<String, Attack> attackSet = {};
   List<Effect> effectSet = [];
   Set<Skill> skillSet = {};
+  Set<WeaponType> weaponProficiencies = {};
   Map<String, int> stats = {};
   int hp = -1;
   int sta = -1;
