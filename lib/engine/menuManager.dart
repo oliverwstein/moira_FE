6351fc5cfcfb634397fd4df146f553f50b1b5b3e
oliverwstein <oliversteininc@gmail.com>
2024-01-19 20:24:04 -0500

    Created the MenuManager and Menu classes.

---
+++ lib/engine/menuManager.dart
@@ -0,0 +1,32 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:moira/content/content.dart';
+
+class MenuManager extends Component with HasGameReference<MoiraGame> implements InputHandler {
+  final List<Menu> _menuStack = [];
+
+  void pushMenu(Menu menu) {
+    _menuStack.add(menu);
+    menu.open();
+  }
+
+  void popMenuState() {
+    _menuStack.removeLast().close();
+  }
+
+  @override
+  KeyEventResult handleKeyEvent(RawKeyEvent key, Set<LogicalKeyboardKey> keysPressed) {
+   return _menuStack.last.handleKeyEvent(key, keysPressed);
+  }
+}
+
+class Menu extends Component with HasGameReference<MoiraGame> implements InputHandler {
+  @override
+  KeyEventResult handleKeyEvent(RawKeyEvent key, Set<LogicalKeyboardKey> keysPressed) {
+    // TODO: implement handleKeyEvent
+    throw UnimplementedError();
+  }
+  void open() {}
+  void close() {}
+}
