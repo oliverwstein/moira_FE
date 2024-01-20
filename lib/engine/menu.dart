9b35b9832e9428650fdc2603a915e94c030b3fea
oliverwstein <oliversteininc@gmail.com>
2024-01-20 10:29:59 -0500

    Fixed direction order in arden and sigurd spritesheets.

--- lib/engine/menu.dart
+++ lib/engine/menu.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,15 +13,17 @@ class MenuManager extends Component with HasGameReference<MoiraGame> implements 
   void pushMenu(Menu menu) {
     _menuStack.add(menu);
     add(menu);
+    debugPrint("push ${_menuStack.lastOrNull} to _menuStack");
     menu.open();
   }
 
   void popMenu() {
+    debugPrint("pop ${_menuStack.lastOrNull} from _menuStack");
     remove(_menuStack.removeLast());
+    debugPrint("top of _menuStack is now: ${_menuStack.lastOrNull}");
   }
   @override
   KeyEventResult handleKeyEvent(RawKeyEvent key, Set<LogicalKeyboardKey> keysPressed) {
-    debugPrint("top of _menuStack is: ${_menuStack.firstOrNull}");
     if (isNotEmpty){
       if(key is RawKeyDownEvent) return _menuStack.last.handleKeyEvent(key, keysPressed);
       return KeyEventResult.handled;
@@ -31,7 +35,7 @@ class MenuManager extends Component with HasGameReference<MoiraGame> implements 
           if(tile.isOccupied) {
             tile.unit!.findReachableTiles(tile.unit!.movementRange.toDouble());
             // add the MoveMenu to the stack.
-            pushMenu(MoveMenu());
+            pushMenu(MoveMenu(tile.unit!, tile.point));
             return KeyEventResult.handled;
           } else {
             // add the GameMenu to the stack.
@@ -57,23 +61,53 @@ abstract class Menu extends Component with HasGameReference<MoiraGame> implement
 }
 
 class MoveMenu extends Menu {
+  final Unit unit;
+  final Point<int> startPoint;
+
+  MoveMenu(this.unit, this.startPoint);
 
   @override 
   Future<void> onLoad() async {
-
+    SpriteAnimation newAnimation = unit.animationMap["left"]!.animation!;
+    unit.sprite.animation = newAnimation;
   }
   @override
+  void onRemove() {
+    super.onRemove();
+    SpriteAnimation newAnimation = unit.animationMap["idle"]!.animation!;
+    unit.sprite.animation = newAnimation;
+  }
+
+  @override
   KeyEventResult handleKeyEvent(RawKeyEvent key, Set<LogicalKeyboardKey> keysPressed) {
+    Point<int> direction = const Point(0, 0);
     switch (key.logicalKey) {
       case LogicalKeyboardKey.keyA:
+        if(game.stage.tileMap[game.stage.cursor.tilePosition]!.state == TileState.move){
+          // Move the unit to the tile selected by the cursor. 
+          unit.moveTo(game.stage.cursor.tilePosition);
+          game.stage.blankAllTiles();
+          close();
+        }
         return KeyEventResult.handled;
       case LogicalKeyboardKey.keyB:
         game.stage.blankAllTiles();
         close();
         return KeyEventResult.handled;
-      default:
-        return KeyEventResult.handled;
+      case LogicalKeyboardKey.arrowLeft:
+        direction = Point(direction.x - 1, direction.y);
+      case LogicalKeyboardKey.arrowRight:
+        direction = Point(direction.x + 1, direction.y);
+      case LogicalKeyboardKey.arrowUp:
+        direction = Point(direction.x, direction.y - 1);
+      case LogicalKeyboardKey.arrowDown:
+        direction = Point(direction.x, direction.y + 1);
     }
+    if (direction != const Point(0,0)){
+        Point<int> newTilePosition = Point(game.stage.cursor.tilePosition.x + direction.x, game.stage.cursor.tilePosition.y + direction.y);
+        game.stage.cursor.moveTo(newTilePosition);
+      }
+      return KeyEventResult.handled;
   }
 
 }
