8c7e2801669212c200004a213cb5efcaba5f7b36
oliverwstein <oliversteininc@gmail.com>
2024-02-04 19:25:51 -0500

    Added grayscale class sprites.

--- lib/content/class.dart
+++ lib/content/class.dart
@@ -1,9 +1,18 @@
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/rendering.dart';
 import 'package:flame/sprite.dart';
 import 'package:flutter/foundation.dart';
 import 'package:moira/content/content.dart';
+final swapRedBluePaint = Paint()
+  ..colorFilter = const ColorFilter.matrix([
+    // R  G  B  A  Const
+    0, 0, 1, 0, 0, // Red output (from blue input)
+    0, 1, 0, 0, 0, // Green output (unchanged)
+    1, 0, 0, 0, 0, // Blue output (from red input)
+    0, 0, 0, 1, 0, // Alpha output (unchanged)
+  ]);
 class Class extends SpriteAnimationComponent with HasGameReference<MoiraGame>{
   final String name;
   final String description;
@@ -77,3 +86,4 @@ class Class extends SpriteAnimationComponent with HasGameReference<MoiraGame>{
     size = spriteSize; anchor = Anchor.center;
   }
 }
+
