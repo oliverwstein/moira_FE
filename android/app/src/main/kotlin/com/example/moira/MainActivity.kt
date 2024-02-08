a7de7b380b520a952a36d109807fd4711c9d97a2
oliverwstein <oliversteininc@gmail.com>
2024-02-08 18:34:23 -0500

    Just the build

--- android/app/src/main/kotlin/com/example/moira/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moira
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- old/content/content.dart
+++
@@ -1,8 +0,0 @@
-export '../engine/engine.dart';
-export 'attack.dart';
-export 'class.dart';
-export 'item.dart';
-export 'skill.dart';
-export 'unit.dart';
-export 'weapon.dart';
-export 'title_card.dart';
